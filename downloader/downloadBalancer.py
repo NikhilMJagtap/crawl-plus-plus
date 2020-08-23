88111f3c5ebefa952a756db3691881b78aa70e70
NikhilMJagtap <nikhil.jagtap@workindia.in>
2020-08-23 16:01:36 +0530

    added lock on queue and tested scalar

--- downloader/downloadBalancer.py
+++ downloader/downloadBalancer.py
@@ -1,9 +1,10 @@
 from xmlrpc.server import SimpleXMLRPCServer
-from downloader.downloader import Downloader
+from downloader.downloader import Downloader, DownloaderBusyException
 from downloader.endpoint import Endpoint
 from collections import deque
 from config import DownloadBalancerConfig as DBC
 import threading
+import time
 
 class DownloadBalancer():
     def __init__(self, host, port):
@@ -12,6 +13,7 @@ class DownloadBalancer():
         self.port = port
         self.rpc_server = SimpleXMLRPCServer((host, port), logRequests=True)
         self.queue = deque()
+        self.queue_lock = threading.Lock()
         self.util = dict()
         for _ in range(DBC["DOWNLOADER_INIT_COUNT"]):
             d = Downloader()
@@ -21,7 +23,6 @@ class DownloadBalancer():
         self.scalar_thread = threading.Thread(target=self.scalar).start()
         self.rpc_thread = threading.Thread(target=self.start_rpc_server).start()
         self.is_ready = True
-        
         # register RPC functions here
         self.rpc_server.register_function(self.download)
 
@@ -30,25 +31,35 @@ class DownloadBalancer():
         return self.balancer(endpoint)
 
     def balancer(self, endpoint):
-        # try:
-        d = self.queue.popleft()
-        if type(endpoint) == dict:
-            endpoint = self.dict_to_endpoint(endpoint)
-        threading.Thread(target=self.download_util, args=(d, endpoint)).start()
-        self.queue.append(d)
-        # except:
-        #     print("To be handled")
+        self.queue_lock.acquire()
+        try:
+            d = self.queue.popleft()
+            print(f"Try downloading {id(d)}")
+            if type(endpoint) == dict:
+                endpoint = self.dict_to_endpoint(endpoint)
+            threading.Thread(target=self.download_util, args=(d, endpoint)).start()
+            self.queue.append(d)
+        except:
+            print("To be handled")
+        self.queue_lock.release()
 
 
-    def download_util(self, d, endpoint):
-        resposne = d.download(endpoint)
-        # TODO: assert for response
-        self.save_response(resposne)
+    def download_util(self, d, endpoint, wait=1):
+        try:
+            resposne = d.download(endpoint)
+            # TODO: assert for response
+            self.save_response(resposne)
+        except DownloaderBusyException:
+            # exponential backoff
+            if wait > 32:
+                print("Download timed out!")
+                return
+            time.sleep(wait*2)
+            self.download_util(d, endpoint, wait=wait*2)
 
 
     def save_response(self, response):
         print("TODO: save response")
-        pass
 
     def start_rpc_server(self):
         try:
@@ -58,24 +69,29 @@ class DownloadBalancer():
             print("\nExiting...")
 
     def scalar(self):
-        for d in self.queue:
-            if d.is_busy:
-                self.util[id(d)].append(1)
-            else:
-                self.util[id(d)].append(0)
-            if sum(self.util[id(d)]) > DBC["DOWNLOADER_UTIL_THRESH"]:
-                # load on downloader d
-                if self.d_count < DBC["DOWNLOADER_MAX_COUNT"]:
-                    # scale up
-                    old_queue = self.util[id(d)]
-                    del old_queue
-                    self.util[id(d)] = deque(maxlen=DBC["DOWNLOADER_UTIL_COUNT"])
-                    new_d = Downloader()
-                    self.util[id(new_d)] = deque(maxlen=DBC["DOWNLOADER_UTIL_COUNT"])
-                    self.queue.append(new_d)
-                    self.d_count += 1
-            else:
-                pass
+        while True:
+            time.sleep(1)
+            self.queue_lock.acquire()
+            for d in self.queue:
+                if d.is_busy():
+                    self.util[id(d)].append(1)
+                else:
+                    self.util[id(d)].append(0)
+                if sum(self.util[id(d)]) > DBC["DOWNLOADER_UTIL_THRESH"]:
+                    print("Too many requests yo! Need help!")
+                    # load on downloader d
+                    if self.d_count < DBC["DOWNLOADER_MAX_COUNT"]:
+                        # scale up
+                        old_queue = self.util[id(d)]
+                        del old_queue
+                        self.util[id(d)] = deque(maxlen=DBC["DOWNLOADER_UTIL_COUNT"])
+                        new_d = Downloader()
+                        self.util[id(new_d)] = deque(maxlen=DBC["DOWNLOADER_UTIL_COUNT"])
+                        self.queue.append(new_d)
+                        self.d_count += 1
+                else:
+                    pass
+            self.queue_lock.release()
 
     def dict_to_endpoint(self, endpoint):
         return Endpoint(
@@ -88,5 +104,5 @@ class DownloadBalancer():
         )
         
     def __del__(self):
-        self.scalar_thread.join()
+        self.scalar_thread.stop()
         self.rpc_thread._stop()
--- downloader/downloader.py
+++ downloader/downloader.py
@@ -11,7 +11,7 @@ class DownloaderBusyException(Exception):
 
 class Downloader:
     def __init__(self):
-        self.is_busy = False
+        self.busy = False
         self.is_healthy = True
         self.attempt_count = 0
         self.data = None
@@ -19,12 +19,11 @@ class Downloader:
         print(f"Downloader {id(self)} up and running!")
 
     def download(self, endpoint):
-        print(f"Download {id(self)} starting at {time.time()}")
         assert isinstance(endpoint, Endpoint), f"endpoint should be Endpoint object, found {type(endpoint)}"
         self.endpoint = endpoint
-        if self.is_busy:
+        if self.busy:
             raise DownloaderBusyException(f"Downloader {id(self)} busy.")
-        self.is_busy = True
+        self.busy = True
         url = endpoint.get_full_url()
         try:
             response = requests.get(url, data=endpoint.get_data(), headers=endpoint.get_headers(), cookies=endpoint.get_cookies())
@@ -40,8 +39,6 @@ class Downloader:
         self.data = response
         self.busy = False
         self.endpoint = None
-        print(response)
-        print(f"Download {id(self)} closing at {time.time()}")
         return response
 
     def handle_failed_download(self):
@@ -49,7 +46,7 @@ class Downloader:
         raise NotImplementedError()
 
     def is_busy(self):
-        return self.is_busy
+        return self.busy
 
 
 
